6a433118909eb4628b1dd4a5d2244802829299ef
rguichette <ralphgui@gmail.com>
2022-05-16 11:51:43 -0400

    added rotation

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useLoader } from '@react-three/fiber'
+import React, {useRef} from 'react'
+import { useFrame, useLoader } from '@react-three/fiber'
 import EarthClouds from '../../textures/earth_clouds.jpg'
 import EarthDayMap from '../../textures/earth_daymap.jpg'
 import EarthNightMap from '../../textures/earth_nightmap.jpg'
@@ -10,22 +10,32 @@ import { TextureLoader } from 'three'
 import * as THREE from 'three'
 
 import { OrbitControls, Stars } from '@react-three/drei'
+import { PointLight } from 'three'
 
 export default function Earth() {
     const [colorMap, normalMap, specularMap, cloudsMap ] = useLoader(TextureLoader, [EarthDayMap, EarthNormalMap, EarthSpecularMap,EarthClouds])
+let ref = useRef()
+let cloudRef = useRef()
+
+useFrame(({clock})=>{
+  let eTime = clock.getElapsedTime()/6;
+  ref.current.rotation.y = eTime
+  cloudRef.current.rotation.y = eTime
+})
+
 
   return (
     <>
-    <ambientLight intensity={0.5}/>
-    <Stars/>
-    <mesh>
+    <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={1.2}/>
+    <Stars fade/>
+    <mesh ref={cloudRef}>
     <sphereGeometry args={[1.007, 32,32]}/>
     <meshPhongMaterial map={cloudsMap} opacity={0.4} depthWrite={true} transparent={true} side={THREE.DoubleSide}/>
     </mesh>
-    <mesh>
+    <mesh ref={ref}>
         <sphereGeometry args={[1, 32,32]}/>
         <meshPhongMaterial specularMap={specularMap} />
-        <meshStandardMaterial map={colorMap} normalMap={normalMap}/>
+        <meshStandardMaterial map={colorMap} normalMap={normalMap} metalness={0.335} roughness={0.7}/>
         <OrbitControls />
     </mesh>
     
